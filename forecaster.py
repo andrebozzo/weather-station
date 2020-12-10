c4688beb80c03e25e5a07f4514939d5d3101e877
andrebozzo <andre1bozzo@gmail.com>
2020-12-10 16:27:56 +0100

    meteo test

--- forecaster.py
+++ forecaster.py
@@ -1,6 +1,7 @@
+import math
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
+import statsmodels.tsa.seasonal as seasonal
 
 class Rilevazione:
     def __init__(self, temp, press, hum, timestamp):
@@ -13,21 +14,51 @@ class ZambrettiForecaster:
     
     def __init__(self, h=981):
         self.h = h
-        self.data = []
+        self.pressList = []
+        self.timestampList = []
+        self.tempList = []
+        self.humList = []
 
     def addData(self, rilevazione):
-        self.data.append(rilevazione)
+        self.pressList.append(rilevazione.press)
+        self.timestampList.append(rilevazione.timestamp)
+        self.tempList.append(rilevazione.temp)
+        self.humList.append(rilevazione.hum)
+        if len(self.pressList) > (96*2):
+            self.pressList.pop(0)
+            self.timestampList.pop(0)
+            self.tempList.pop(0)
+            self.humList.pop(0)
+
+    def trendDetector(self, index, data, order=1):
+        result = np.polyfit(index, list(data), order)
+        slope = result[-2]
+        return float(slope)
 
     def forecast(self):
         print("starting forecast")
-        P0 = self.data[-1].press*(1-(0.0065 * self.h)/(self.data[-1].temp + 0.0065 * self.h + 273.15))
-        pressureValues = pd.DataFrame()
-        for d in self.data:
-            pressureValues["y"] = d.press
-            pressureValues["ts"] = d.timestamp
-        decomposition = sm.tsa.seasonal_decompose(pressureValues)
-        fig1 = decomposition.plot()
+        print(self.pressList[-1])
+        P0 = self.pressList[-1]* math.pow((1-((0.0065 * self.h)/(self.tempList[-1] + 0.0065 * self.h + 273.15))), -5.257)
+        index = range(len(self.pressList))
+        pressureTrend = self.trendDetector(index, self.pressList)
+        z = 0
+        if pressureTrend > 1:
+            z = 130 - P0 / 81
+        elif pressureTrend >= -1 and pressureTrend <= 1:
+            z = 147 - (5*P0)/376
+        else:
+            z = 179 - (2*P0)/129
+            
+        #adjustment for wind skipped
+        '''
+        if(winddir = N)
+            z = z + 1
+        else:
+            z = z - 2
+        '''
+        print(z)
+    
 
-
+    
     
 
--- forecaster_tester.py
+++ forecaster_tester.py
@@ -1,9 +1,14 @@
 from forecaster import ZambrettiForecaster
 from forecaster import Rilevazione
 from datetime import datetime
+import math
 
 f = ZambrettiForecaster()
-for i in range(100):
-    r = Rilevazione(10, 990+i, 50, datetime.now())
-    f.addData(r)
+p = 990
+for i in range(7):
+    for j in range(24):
+        for k in range(4):
+            r = Rilevazione(10, p+0.01, 50, datetime(2020, 1, i+1, j, k*15, 0, 0))
+            p = p +0.01
+            f.addData(r)
 f.forecast()
