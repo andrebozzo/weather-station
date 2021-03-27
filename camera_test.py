5184f0f3b401a27bc5fb93c2abd47d3a81df9d4a
Andrea Bozzoni <andre1bozzo@gmail.com>
2021-03-27 19:04:57 +0100

    updated meteo updates

--- camera_test.py
+++ camera_test.py
@@ -2,8 +2,18 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-
+camera.resolution = (1920, 1080)
+camera.framerate = 15
+camera.contrast = 50
+camera.brightness = 50
 camera.start_preview()
 sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
+'''
+for awb in camera.AWB_MODES:
+	camera.awb_mode = awb
+	camera.annotate_text = "awb mode: %s" % awb
+	sleep(5)
+	camera.capture("/home/pi/Desktop/AWBphoto%s.jpg" % awb)
+'''
+camera.capture("/home/pi/Desktop/snapshot.jpg")
 camera.stop_preview()
--- meteo_send_updates.py
+++ meteo_send_updates.py
@@ -8,15 +8,13 @@ from datetime import datetime
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera(resolution=(1280, 720))
-
 url_data = "https://meteo-station.herokuapp.com/data"
 url_photo = "https://meteo-station.herokuapp.com/photo"
 
 #url_data = "http://192.168.1.80:8080/data"
 #url_photo = "http://192.168.1.80:8080/photo"
 
-interval = 120 #120 seconds (2 mins)
+interval = 1800 #1800 seconds (30 mins)
 if len(sys.argv) > 1:
     interval =int(sys.argv[1])
 print("Updates will be sent every %d seconds, every %.2f minutes" % (interval, interval/60))
@@ -29,14 +27,13 @@ calibration_params = bme280.load_calibration_params(bus, address)
 # the sample method will take a single reading and return a
 # compensated_reading object
 temp_sonda = DS18B20()
-
+camera = PiCamera(resolution=(1080, 920))
 # the compensated_reading class has the following attributes
 while True:
 
     print("Receiving data from bme280...")
     data = bme280.sample(bus, address, calibration_params)
     # there is a handy string representation too
-    print(data)
     data_id = data.id
     ts = datetime.now()
     temp = round(data.temperature, 1)
@@ -64,7 +61,7 @@ while True:
 
     try:
         photo_path = '/home/pi/Desktop/snapshot.jpg'
-        camera.awb_mode = "sun"
+        camera.awb_mode = "sunlight"
         camera.start_preview()
         sleep(5)
         camera.capture(photo_path)
